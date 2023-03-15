f1d507b73dcd4d3bca9daa970b595f042fbcb414
FailX <xaibullin.96@mail.ru>
2023-03-15 09:10:58 +0300

    add TestTimeWorkLinkedList

--- IncertPerfomance/Program.cs
+++ IncertPerfomance/Program.cs
@@ -2,20 +2,40 @@
 //На примере этого текста, выясните, какие будут различия между этими коллекциями.
 using System.IO;
 using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
 
+TestTimeWorkList();
+TestTimeWorkLinkedList();
 
+static void TestTimeWorkList()
+{
     string path = @"C:\Users\Fail\Desktop\Modul\IncertPerfomance\Text1.txt";
     FileInfo fileInfo = new FileInfo(path);
-    
-
-if (fileInfo.Exists)
-{
-
-    Console.WriteLine($"Имя файла: {fileInfo.Name}");
-    Console.WriteLine($"Время создания: {fileInfo.CreationTime}");
-    Console.WriteLine($"Размер: {fileInfo.Length}");
-
-    string contents = File.ReadAllText(@"C:\Users\Fail\Desktop\Modul\IncertPerfomance\Text1.txt");
-    var contents = new List<contents>();
+    if (fileInfo.Exists)
+    {
+        Console.WriteLine(fileInfo.Name);
+        var timer = new Stopwatch();
+        timer.Start();
+        var contents = File.ReadAllText(@"C:\Users\Fail\Desktop\Modul\IncertPerfomance\Text1.txt");
+        var contentList = new List<string>();
+        contentList.Add(contents);
+        timer.Stop();
+        Console.WriteLine($"Time work in millisecond for List: {timer.ElapsedMilliseconds}");
+    }
 }
-
+static void TestTimeWorkLinkedList()
+{
+    string path = @"C:\Users\Fail\Desktop\Modul\IncertPerfomance\Text1.txt";
+    FileInfo fileInfo = new FileInfo(path);
+    if (fileInfo.Exists)
+    {
+        var timer = new Stopwatch();
+        timer.Start();
+        var contents = File.ReadAllText(@"C:\Users\Fail\Desktop\Modul\IncertPerfomance\Text1.txt");
+        var contentList = new List<string>();
+        LinkedList<string> Llist = new LinkedList<string>(contentList);
+        timer.Stop();
+        Console.WriteLine($"Time work in millisecond for LinkedList: {timer.ElapsedMilliseconds}");
+    }
+}
